cf209518067339f6011ad62d06bdfeede565d28e
hafideveloper <hafideveloperr@gmail.com>
2024-07-09 16:54:42 +0300

    logo added

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 
 const SplashScreen = ({ navigation }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       navigation.replace('HomeTabs'); 
-    }, 5000);
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, [navigation]);
 
   return (
     <View style={styles.container}>
+      <Image source={require('../../assets/logo.png')} style={styles.logo} />
     </View>
   );
 };
@@ -22,6 +23,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
+  },
+  logo: {
+    bottom:100,
+    width: 200, 
+    height: 200, 
+    resizeMode: 'contain', 
   },
 });
 
